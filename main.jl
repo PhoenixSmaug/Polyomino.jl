3ad01ffa514343ef62babe6326491ad0b5676d57
Christoph Muessig <account@miamuessig.de>
2022-02-22 23:58:56 +0100

    implement minimal line cover, add presentation

--- main.jl
+++ main.jl
@@ -1,8 +1,25 @@
 include("polyomino.jl")
 
+println("<Polyomino Generation (n = 50)>")
+
+println("Shuffling (p = 0):")
+show(Polyomino(50, 0.0, true))
+
+println("Shuffling (p = 0.8):")
+show(Polyomino(50, 0.8, true))
+
+println("Eden Model:")
+show(Polyomino(50, 0.0, false))
+
+
+println("<Polyomino Analysis>")
 polyomino = Polyomino(50, 0.6, true)
 
-printMaxRooks(polyomino)
-println()
+println("Minimal Rook Setup (Greedy)")
 printMinRooks(polyomino)
 
+println("Maximal Rook Setup")
+printMaxRooks(polyomino)
+
+println("Minimal Line Cover")
+printMinLineCover(polyomino)
