b82f74ebfc59150ad9a2ca4ce9df02daaf9d9717
Christoph Muessig <44726536+PhoenixSmaug@users.noreply.github.com>
2022-02-23 22:17:10 +0100

    change main example, fix typo

--- main.jl
+++ main.jl
@@ -1,19 +1,19 @@
 include("polyomino.jl")
 
-println("<Polyomino Generation (n = 40)>")
+println("<Polyomino Generation (n = 30)>")
 
 println("Shuffling (p = 0):")
-show(Polyomino(40, 0.0, true))
+show(Polyomino(30, 0.0, true))
 
 println("Shuffling (p = 0.8):")
-show(Polyomino(40, 0.8, true))
+show(Polyomino(30, 0.8, true))
 
 println("Eden Model:")
-show(Polyomino(40, 0.0, false))
+show(Polyomino(30, 0.0, false))
 
 
 println("<Polyomino Analysis>")
-polyomino = Polyomino(40, 0.6, true)
+polyomino = Polyomino(30, 0.6, true)
 
 println("Minimal Rook Setup")
 printMinRooks(polyomino)
